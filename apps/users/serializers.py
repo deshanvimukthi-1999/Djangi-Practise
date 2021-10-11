17c47c16ac26b6b1a0585c43d443661dcba82a14
Deshan Vimukthi <vimudeshan123@gmail.com>
2021-10-11 11:13:43 +0530

    Updates

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,26 +1,14 @@
 from django.contrib.auth import get_user_model
-from django.template.backends import django
 from rest_framework import serializers
 
 from apps.users.models import Company
-
-
-def create_user(validated_data):
-    validated_data.pop('confirm_password')
-    validated_data['username'] = validated_data['email']
-    instance = get_user_model().objects.create(**validated_data)
-    instance.set_password(validated_data['password'])
-    instance.save()
-    return instance
 
 
 class AuthRegisterSerializer(serializers.Serializer):
     first_name = serializers.CharField(required=True)
     last_name = serializers.CharField(required=True)
     email = serializers.CharField(required=True)
-    company = serializers.CharField(required=False)
     password = serializers.CharField(required=True)
-    confirm_password = serializers.CharField(required=True)
 
 
 class CompanySerializer(serializers.ModelSerializer):
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
@@ -6,39 +7,35 @@ from rest_framework.viewsets import ViewSet, ModelViewSet
 
 from apps.users.models import User
 from apps.users.serializers import AuthRegisterSerializer, UserSerializer
-import logging
 
-
-logger = logging.getLogger(__name__)
+from apps.users.services import register_user, delete_user
 
 
 class AuthViewSet(ViewSet):
 
-    @action(methods=['post'], detail=False)
+    @action(methods=['post'], detail=False, permission_classes=[], url_path='register', url_name='register')
     def register(self, request):
         serializer = AuthRegisterSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
-            serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+            user = register_user(request.data)
+            serializer = UserSerializer(user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-    def get_permissions(self):
-        if self.action == 'me':
-            permission_classes = [IsAuthenticated]
-        else:
-            permission_classes = [IsAuthenticated, IsAdminUser]
-
-        return [permission() for permission in permission_classes]
-
     @action(methods=['get'], detail=False, permission_classes=[IsAuthenticated], url_path='me', url_name='me')
     def me(self, request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def perform_destroy(self, instance):
-        instance.is_active = False
-        instance.save()
+    def destroy(self, request, pk=None, *args, **kwargs):
+        delete_user(pk)
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+    def get_queryset(self):
+        return User.objects.filter(
+            Q(company=self.request.user.company) & Q(is_active=True)
+        )
